3d7c0398bfee33f3646fb8e2dbf0f2ffe1434d1c
Jeff-amaro <jeff.amaro@tecsup.edu.pe>
2026-08-26 23:18:17 -0500

    aplicar polimorfismo con la interfaz EstrategiaDescuento

--- semana 2/app/src/main/java/com.amaro.lab02carritokotlin/Carrito.kt
+++ semana 2/app/src/main/java/com.amaro.lab02carritokotlin/Carrito.kt
@@ -42,3 +42,27 @@ class ProductoDigital(
     cantidad: Int,
     val urlDescarga: String
 ) : Producto(nombre, precio, cantidad)
+
+interface EstrategiaDescuento {
+    fun calcularDescuento(montoTotal: Double): Double
+    fun obtenerNombre(): String
+}
+
+class DescuentoPorMonto : EstrategiaDescuento {
+    override fun calcularDescuento(montoTotal: Double): Double {
+        return when {
+            montoTotal > 5000 -> montoTotal * 0.10
+            montoTotal > 3000 -> montoTotal * 0.05
+            else -> 0.0
+        }
+    }
+
+    override fun obtenerNombre(): String {
+        return "Descuento por Monto Elevado"
+    }
+}
+
+class SinDescuento : EstrategiaDescuento {
+    override fun calcularDescuento(montoTotal: Double): Double = 0.0
+    override fun obtenerNombre(): String = "Sin Descuento"
+}
