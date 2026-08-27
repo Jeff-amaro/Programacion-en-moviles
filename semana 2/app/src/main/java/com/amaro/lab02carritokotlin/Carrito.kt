8f80296bfce236c7622bd39dd47b86de1fa5922c
Jeff-amaro <jeff.amaro@tecsup.edu.pe>
2026-08-26 21:28:24 -0500

    Agrega reporte de detalle con columnas alineadas

--- semana 2/app/src/main/java/com/amaro/lab02carritokotlin/Carrito.kt
+++ semana 2/app/src/main/java/com/amaro/lab02carritokotlin/Carrito.kt
@@ -22,6 +22,17 @@ fun calcularTotal(subtotal: Double, igv: Double): Double {
     return subtotal + igv
 }
 
+fun mostrarDetalle(productos: List<Producto>) {
+    println("--------- DETALLE DEL CARRITO ---------")
+    var i = 1
+    for (p in productos) {
+        val importe = p.precio * p.cantidad
+        println(String.format("%d. %-20s x%d  S/ %8.2f", i, p.nombre, p.cantidad, importe))
+        i++
+    }
+    println("---------------------------------------")
+}
+
 fun main() {
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP    ")
@@ -40,10 +51,9 @@ fun main() {
     carrito.add(Producto("Mouse Logitech", 45.5, 2))
     carrito.add(Producto("Audifonos Sony", 120.0, 1))
 
+    mostrarDetalle(carrito)
 
-    for (producto in carrito) {
-        println("Producto agregado: ${producto.nombre}")
-    }
+    println("Cantidad de productos: ${carrito.size}")
     println()
 
     val subtotal = calcularSubtotal(carrito)
