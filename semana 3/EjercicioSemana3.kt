d42d80119de734546250850d128ab989a045adeb
Jeff-amaro <jeff.amaro@tecsup.edu.pe>
2026-09-01 21:47:09 -0500

    agregue la validacion de las cuotas y los calculos para la boleta

--- semana 3/EjercicioSemana3.kt
+++ semana 3/EjercicioSemana3.kt
@@ -1,6 +1,6 @@
 import java.util.Scanner
 
-fun main(){
+fun main() {
     val scanner = Scanner(System.`in`)
     println("LIBRERIA")
 
@@ -14,5 +14,38 @@ fun main(){
     val cantidad = scanner.nextInt()
 
     println("Datos registrados:")
-    println("Producto: $nombreProducto  Precio: S/ $precio | Cantidad: $cantidad")
+    println("Producto: $nombreProducto | Precio: S/ $precio | Cantidad: $cantidad")
+
+    val subtotal = precio * cantidad
+    println("\nSubtotal a financiar: S/ $subtotal")
+
+    var cuotas: Int
+    var porcentajeInteres: Double
+
+    do {
+        println("\n  Opciones de financiamiento  ")
+        println(" 6 cuotas (20% interes)")
+        println(" 12 cuotas (40% interes)")
+        println(" 24 cuotas (60% interes)")
+        println("Ingrese las cantidad de cuotas: ")
+        cuotas = scanner.nextInt()
+
+        porcentajeInteres = when (cuotas) {
+            6 -> 0.20
+            12 -> 0.40
+            24 -> 0.60
+            else -> {
+                println("opcion invalida, ingrese un numero correcto")
+                0.0
+            }
+        }
+    } while (porcentajeInteres == 0.0)
+    val montoInteres = subtotal * porcentajeInteres
+    val totalPagar = subtotal + montoInteres
+    val pagoMensual = totalPagar / cuotas
+
+    println("\n  Boleta del Credito: ")
+    println("Interes aplicado: ${(porcentajeInteres * 100).toInt()}%(S/ $montoInteres)")
+    println("Total a pagar: S/$totalPagar")
+    println("Pago mensual: S/$pagoMensual")
 }
