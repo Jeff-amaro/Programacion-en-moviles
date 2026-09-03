c34670ae7565a7f5936d90bcccf84653252ed9b1
Jeff-amaro <jeff.amaro@tecsup.edu.pe>
2026-09-02 21:41:36 -0500

    Aplica reglas de diseno y mensaje de confirmacion

--- semana 3/Lab03RegistroProducto/app/src/main/java/com/amaro/lab03registroproducto/MainActivity.kt
+++ semana 3/Lab03RegistroProducto/app/src/main/java/com/amaro/lab03registroproducto/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.amaro.lab03registroproducto.ui.theme.Lab03RegistroProductoTheme
@@ -120,7 +121,16 @@ fun PantallaRegistro(modifier: Modifier = Modifier) {
                         fontWeight = FontWeight.Bold,
                         color = MaterialTheme.colorScheme.primary
                     )
-                }}
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "Producto registrado correctamente",
+                color = Color(0xFF2E7D32),
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
